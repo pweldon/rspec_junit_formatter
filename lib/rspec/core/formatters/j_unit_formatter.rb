4f7ce14e9b94141882269d06507c554a53081a44
Peter Weldon <peter.weldon@null.net>
2011-10-14 10:15:59 -0700

    Make each spec file a testsuite.

--- lib/rspec/core/formatters/j_unit_formatter.rb
+++ lib/rspec/core/formatters/j_unit_formatter.rb
@@ -4,44 +4,57 @@ require 'time'
 # Based on XML schema: http://windyroad.org/dl/Open%20Source/JUnit.xsd
 class RSpec::Core::Formatters::JUnitFormatter < RSpec::Core::Formatters::BaseFormatter
   def xml
-    @xml ||= Builder::XmlMarkup.new :target => output, :indent => 2 
+    @xml ||= Builder::XmlMarkup.new :target => output, :indent => 2
   end
-  
+
   def start example_count
     @start = Time.now
     super
   end
-  
+
   def dump_summary duration, example_count, failure_count, pending_count
     super
-    
+
+    examples_by_path = examples.group_by { |example| example.file_path }
+    common_prefix_length = examples_by_path.keys.map { |path| path.split('/').size }.min - 2
+    common_prefix_length = 0 if common_prefix_length < 0
+
     xml.instruct!
-    xml.testsuite :tests => example_count, :failures => failure_count, :errors => 0, :time => '%.6f' % duration, :timestamp => @start.iso8601 do
-      xml.properties
-      examples.each do |example|
-        send :"dump_summary_example_#{example.execution_result[:status]}", example
+    xml.testsuites do
+      examples_by_path.each do |path, examples|
+        xml.testsuite(:tests => examples.size,
+                      :failures => examples.count { |e| e.execution_result[:status] != 'passed' },
+                      :package => path.gsub(/.rb$/,'').split('/').drop(common_prefix_length).join('.'),
+                      :errors => 0,
+                      :time => '%.6f' % duration,
+                      :timestamp => @start.iso8601) do
+          xml.properties
+          examples.each do |example|
+            send :"dump_summary_example_#{example.execution_result[:status]}", example
+          end
+        end
       end
     end
   end
-  
+
   def xml_example example, &block
-    xml.testcase :classname => example.file_path, :name => example.full_description, :time => '%.6f' % example.execution_result[:run_time], &block
+    xml.testcase :classname => example.example_group.ancestors.reverse.map { |eg| eg.description }.join('.'), :name => example.description, :time => '%.6f' % example.execution_result[:run_time], &block
   end
-  
+
   def dump_summary_example_passed example
     xml_example example
   end
-  
+
   def dump_summary_example_pending example
     xml_example example do
       xml.skipped
     end
   end
-  
+
   def dump_summary_example_failed example
     exception = example.execution_result[:exception]
     backtrace = format_backtrace exception.backtrace, example
-    
+
     xml_example example do
       xml.failure :message => exception.to_s, :type => exception.class.name do
         xml.cdata! "#{exception.message}\n#{backtrace.join "\n"}"
